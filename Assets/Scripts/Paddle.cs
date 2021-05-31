f32e629d55da97675092cda29ba71f2b7c3adb03
Alin Cristian <alin.cristian977@gmail.com>
2021-06-01 00:57:46 +0200

    Clamped X-axis Paddle movement to fit the screen bounds

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    //TODO: Improve by using relative values
     [SerializeField] private float _screenWidthInUnits = 16f;
+    [SerializeField] private float _minX = 1f;
+    [SerializeField] private float _maxX = 15f;
 
     private void Update()
     {
-        var paddleTransform = transform;
+        var paddlePos = transform.position;
         
         float mousePosInUnits = Input.mousePosition.x / Screen.width * _screenWidthInUnits;
-        var paddlePos = new Vector2(mousePosInUnits, paddleTransform.position.y);
-        
-        paddleTransform.position = paddlePos;
+
+        var newPaddlePos = new Vector2(
+            Mathf.Clamp(mousePosInUnits, _minX, _maxX),
+            paddlePos.y
+        );
+
+        transform.position = newPaddlePos;
     }
 }
