0dc10b3a3698849685ba81a2e82ef3b9e135b082
Alin Cristian <alin.cristian977@gmail.com>
2021-06-01 21:44:21 +0200

    Set AudioClip on Block collision

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private AudioClip _breakSound;
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
+        AudioSource.PlayClipAtPoint(_breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
