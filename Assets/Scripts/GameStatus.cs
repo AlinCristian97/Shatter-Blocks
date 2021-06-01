2111a05848e8eb8cf7904415afbd2ab61f4a32d3
Alin Cristian <alin.cristian977@gmail.com>
2021-06-01 23:50:15 +0200

    Added functionality for displaying the score in the UI

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -3,14 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     [Range(0.1f, 10f)] [SerializeField] private float _gameSpeed = 1f;
     [SerializeField] private int _pointsPerBlockDestroyed = 83;
+    [SerializeField] private TextMeshProUGUI _scoreText;
     
     [SerializeField] private int _currentScore = 0; //debug
-    
+
+    private void Start()
+    {
+        _scoreText.text = _currentScore.ToString();
+    }
+
     private void Update()
     {
         Time.timeScale = _gameSpeed;
@@ -19,5 +26,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         _currentScore += _pointsPerBlockDestroyed;
+        _scoreText.text = _currentScore.ToString();
     }
 }
